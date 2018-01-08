b57068f4ae8d3745f18a578d6bc926851fd32b57
Jade Stobbe <jadestobbe@gmail.com>
2018-01-08 20:15:55 +0000

    nav bar will now appear and dissapear when switching to mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 declare var $: any;
 
 @Component({
@@ -11,4 +11,33 @@ export class AppComponent {
   toggleNav(): void {
     $('nav').slideToggle();
   }
+  
+  ngOnInit() {
+    if($('#menubtn').is(':visible')){
+      $('nav').css('display','none');
+    }
+    $('#menubtn').on('click', ()=>{
+      if($('nav').css('display') == 'none'){
+       $('nav').css('display','grid');
+      }
+      else{
+        $('nav').css('display','none');
+      }
+    });
+   $( window ).resize(function() {
+     if($('#menubtn').is(':visible')){
+       $('nav').css('display','none');
+     }
+     else{
+       $('nav').css('display','grid');
+     }
+   });
+  }
 }
+
+// if($('#menubtn').is(':visible')){
+//       alert('visible');
+//     }
+//     else if($('#menubtn').is(':hidden')){
+//       alert('hidden');
+//     }
