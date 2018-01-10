35f9c7cab3088449f2d6a6e5898f390ba8b2d3d3
Jade Stobbe <jadestobbe@gmail.com>
2018-01-10 17:31:53 -0600

    took out unused code

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
 
-const port = process.env.PORT || '5000';
-
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
